803f419a78faf2343b5dc9e14c9d070f5c3f50a2
saba756 <sabaofficial12@gmail.com>
2020-07-21 13:10:24 +0500

    routing is implemented

--- src/app/shop/shop.service.ts
+++ src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import {IBrand} from './../shared/models/brand';
 import {IType} from './../shared/models/productType';
 import {delay, map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 @Injectable({
   providedIn: 'root'
 })
@@ -25,13 +26,15 @@ baseUrl = 'https://localhost:44322/api/';
     params = params.append('sort', shopParams.sort);
     params = params.append('pageIndex', shopParams.pageNumber.toString());
     params = params.append('pageIndex', shopParams.pageSize.toString());
-        
     return this.http.get<IPagination>(this.baseUrl + 'products' , {observe: 'response', params})
     .pipe(
       map(response => {
         return response.body;
       })
-    )
+    );
+  }
+  getProduct(id: number){
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
   getBrand(){
     return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
