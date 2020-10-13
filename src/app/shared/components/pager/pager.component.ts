94a3144378022c89de3cb03e3f8b223ce6d64f30
Saba Tahir <sata@ciklum.com>
2020-10-13 17:36:14 +0500

    implementing cache for products

--- src/app/shared/components/pager/pager.component.ts
+++ src/app/shared/components/pager/pager.component.ts
@@ -8,6 +8,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class PagerComponent implements OnInit {
   @Input() pageSize: number;
   @Input() totalCount: number;
+  @Input() pageNumber: number;
   @Output() pageChanged = new EventEmitter<number>();
   constructor() { }
 
